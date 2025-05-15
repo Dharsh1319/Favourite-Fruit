a6dcbdc60fc8aa01ee8e4995b8a5b1365ae8a149
Dharsh1319 <dharsinimv.ece2023@citchennai.net>
2025-05-15 12:53:34 +0530

    Fruit.html

---
+++ Fruit.js
@@ -0,0 +1,14 @@
+<p>What is your Favourite fruit?</p>
+<label for="fname">Your opinion:</label>
+input type="text" />
+
+<button type= Submit!</button>
+
+<select name="Fruits" id="Fruits">
+  <option value="Apple">Apple‹/option> 
+  <option value="Mango" ›Mango‹/option> 
+  <option value="Kiwi">Kiwi</option> 
+  <option value="Orange" ›Orange‹/option>
+</select>
+
+<img src="Fruits.jpeg" alt="All fruits" width="150" height="200>
